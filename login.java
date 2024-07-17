342f631b91d5e4518cdd2a4c6fd653c86220d173
root <root@ip-172-31-4-157.ap-southeast-2.compute.internal>
2024-07-17 15:49:56 +0000

    added some 4 lines

--- login.java
+++ login.java
@@ -1,5 +1,5 @@
-srikanth is learning the aws
-but aws is very dificult
-so that learning very hard
 hard work is never faild
+failur is good lesson
+lesson is help our life
+sonix is doing good work
 
